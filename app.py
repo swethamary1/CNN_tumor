feb4f196862464e9d5916d0ac49f3220e2787c9c
swethamary1 <146935409+swethamary1@users.noreply.github.com>
2024-09-19 14:02:16 +0530

    app.py

--- app.py
+++ app.py
@@ -1,41 +1,43 @@
 import streamlit as st
+from PIL import Image
 import numpy as np
 import tensorflow as tf
-from PIL import Image
-import cv2
 
-# Load the trained CNN tumor classification model
-model = tf.keras.models.load_model("path_to_your_saved_model/cnn_tumor.h5")
 
-# Function to make predictions
-def make_prediction(image, model):
-    img = Image.open(image)
-    img = img.resize((128, 128))  # Resize to match the model's input size
-    img = np.array(img)
-    img = np.expand_dims(img, axis=0)  # Add batch dimension
-    img = tf.keras.utils.normalize(img, axis=1)  # Normalize the image
 
-    prediction = model.predict(img)
-    return "Tumor Detected" if prediction > 0.5 else "No Tumor"
+def load_model():
+    model = tf.keras.models.load_model('cnn_tumor2.h5')
+    return model
 
-# Streamlit app
-st.title("Tumor Classification with CNN")
-st.write("Upload a medical image for tumor classification using a CNN model.")
+model = load_model()
 
-# Image file upload
-uploaded_file = st.file_uploader("Upload a medical image (JPG/PNG)", type=["jpg", "png"])
 
-# Button to proceed with tumor classification
-if st.button("Classify"):
-    if uploaded_file is not None:
-        # Display the uploaded image
-        image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+def preprocess_image(image):
+    image = image.resize((128,128)) 
+    image = np.array(image) / 255.0  
+    image = np.expand_dims(image, axis=0)  
+    return image
 
-        # Make a prediction
-        result = make_prediction(uploaded_file, model)
 
-        # Display the result
-        st.write(f"Prediction: {result}")
+def predict_tumor(image):
+    predictions = model.predict(image)
+    if predictions[0] > 0.5:  
+        return "Tumorous"
     else:
-        st.write("Please upload an image file.")
+        return "Non-tumorous"
+
+
+st.title("Tumor Detection using CNN")
+
+
+uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    resized_image = image.resize((300, 200))
+    st.image(resized_image, caption="Uploaded Image")
+    
+    processed_image = preprocess_image(image)
+    result = predict_tumor(processed_image)
+    st.info(f"Prediction: {result}")
